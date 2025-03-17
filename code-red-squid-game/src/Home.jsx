1e7748a22cba87f814e21c3f212e1ffb02d6f9bd
Parthiv Chandra Gajula <56657810+Parthiv12@users.noreply.github.com>
2025-03-17 19:04:58 +0000

    working front end v1

---
+++ code-red-squid-game/src/Home.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../public/Home.css";
+
+const Home = () => {
+  const [fadeOut, setFadeOut] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFadeOut(true);
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="home-container">
+      <h1 className={`code-red ${fadeOut ? "fade-out" : ""}`}>CODE RED</h1>
+      {fadeOut && (
+        <div className="buttons-container">
+          <button onClick={() => navigate("/login")}>Login</button>
+          <button onClick={() => navigate("/signup")}>Sign Up</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
